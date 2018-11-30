40a43a37eb8e43a18b23bb64d5128d65ac9c6fff
johnny <johnny@wantgoo.com>
2018-11-30 11:08:09 +0800

    i don't wanna input too much code

---
+++ jquery-http/src/jquery-http.js
@@ -0,0 +1,26 @@
+﻿(function ($) {
+    $.extend({
+        http: {
+            before: function (func) {
+                if (func) func();
+
+                return this;
+            },
+            get: function (url) {
+                return $.ajax(url, { method: "GET" });
+            },
+            post: function (url, data) {
+                return $.ajax(url, { method: "POST", data: data });
+            },
+            put: function (url, data) {
+                return $.ajax(url, { method: "PUT", data: data });
+            },
+            patch: function (url, data) {
+                return $.ajax(url, { method: "PATCH", data: data });
+            },
+            delete: function (url) {
+                return $.ajax(url, { method: "DELETE" });
+            }
+        }
+    });
+})(jQuery);
