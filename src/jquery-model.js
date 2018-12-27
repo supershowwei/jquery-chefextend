4c39c734259d921ef495337f0b090cb1e7750a71
johnny <johnny@wantgoo.com>
2018-12-27 15:17:24 +0800

    upgrade to 0.0.2

--- src/jquery-model.js
+++ src/jquery-model.js
@@ -1,4 +1,43 @@
-﻿(function ($) {
+﻿if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function (search, pos) {
+        return this.substring(!pos || pos < 0 ? 0 : +pos, search.length) === search;
+    };
+}
+
+if (!String.prototype.endsWith) {
+    String.prototype.endsWith = function (search, this_len) {
+        if (this_len === undefined || this_len > this.length) {
+            this_len = this.length;
+        }
+        return this.substring(this_len - search.length, this_len) === search;
+    };
+}
+
+(function ($) {
+    $.expr[":"].attrStartsWith = $.expr.createPseudo(function (filterParam) {
+        return function (element, context, isXml) {
+            for (var i = element.attributes.length - 1; i >= 0; i--) {
+                if (element.attributes[i].name.startsWith(filterParam)) {
+                    return true;
+                }
+            }
+
+            return false;
+        };
+    });
+
+    $.expr[":"].attrEndsWith = $.expr.createPseudo(function (filterParam) {
+        return function (element, context, isXml) {
+            for (var i = element.attributes.length - 1; i >= 0; i--) {
+                if (element.attributes[i].name.endsWith(filterParam)) {
+                    return true;
+                }
+            }
+
+            return false;
+        };
+    });
+
     $.extend({
         jqModel: {
             getValue: function (element) {
@@ -6,22 +45,50 @@
 
                 if ($element.is(":radio")) {
                     return $("input[type='radio'][name='" + element.name + "']:checked").val();
-                } else if ($element.is(":checkbox")) {
-                    return $element.prop("checked");
                 } else {
-                    return element.value === undefined ? $element.text() : $element.val();
+                    return $element.val();
                 }
             },
             setValue: function (element, value) {
                 var $element = $(element);
 
-                if($element.is(":radio")) {
+                if ($element.is(":radio")) {
                     $("input[type='radio'][name='" + element.name + "'][value='" + value + "']").prop("checked", true);
                 } else if ($element.is(":checkbox")) {
                     $element.prop("checked", value);
                 } else {
                     element.value === undefined ? $element.text(value) : $element.val(value);
                 }
+            },
+            getText: function (element) {
+                return $(element).text();
+            },
+            setText: function (element, text) {
+                $(element).text(text);
+            },
+            getChecked: function (element) {
+                return $(element).prop("checked");
+            },
+            setChecked: function (element, checked) {
+                $(element).prop("checked", checked);
+            },
+            setClasses: function (element, classNames) {
+                $(element).attr("class", classNames);
+            },
+            setClass: function (element, className, show) {
+                if (show) {
+                    $(element).addClass(className);
+                } else {
+                    $(element).removeClass(className);
+                }
+            },
+            setStyles: function (element, styles) {
+                $(element).attr("style", styles);
+            },
+            setStyle: function (element, style, value) {
+                console.log(style);
+                console.log(value);
+                $(element).css(style, value);
             },
             toNumber: function (value) {
                 return isNaN(value) ? undefined : Number(value);
@@ -31,9 +98,7 @@
 
     $.fn.extend({
         model: function (setter) {
-            var numberType = "data-prop-number";
-
-            var elements = this.find("[data-prop],[" + numberType + "]");
+            var elements = this.find(":attrStartsWith('c-')");
 
             if (elements.length === 0) return undefined;
 
@@ -42,34 +107,56 @@
 
                 $.each(elements,
                     function (index, element) {
-                        if (element.dataset.propNumber) {
-                            if (!obj.hasOwnProperty(element.dataset.propNumber)) {
-                                obj[element.dataset.propNumber] = $.jqModel.toNumber($.jqModel.getValue(element));
-                            }
-                        } else {
-                            if (!obj.hasOwnProperty(element.dataset.prop)) {
-                                obj[element.dataset.prop] = $.jqModel.getValue(element);
+                        for (var i = 0; i < element.attributes.length; i++) {
+                            var attrName = element.attributes[i].name;
+
+                            if (!attrName.startsWith("c-")) continue;
+
+                            var propName = element.attributes[i].value;
+
+                            if (attrName === "c-value") {
+                                obj[propName] = $.jqModel.getValue(element);
+                            } else if (attrName === "c-value-number") {
+                                obj[propName] = $.jqModel.toNumber($.jqModel.getValue(element));
+                            } else if (attrName === "c-text") {
+                                obj[propName] = $.jqModel.getText(element);
+                            } else if (attrName === "c-text-number") {
+                                obj[propName] = $.jqModel.toNumber($.jqModel.getText(element));
+                            } else if (attrName === "c-checked") {
+                                obj[propName] = $.jqModel.getChecked(element);
                             }
                         }
                     });
 
                 return obj;
             } else {
-                $.each(Object.keys(setter),
-                    function (index, key) {
-                        var i;
+                $.each(elements,
+                    function (index, element) {
+                        for (var i = 0; i < element.attributes.length; i++) {
+                            var attrName = element.attributes[i].name;
 
-                        for (i = 0; i < elements.length; i++) {
-                            if (elements[i].dataset.propNumber === key) {
-                                $.jqModel.setValue(elements[i], Number(setter[key]));
-                                break;
-                            } else if (elements[i].dataset.prop === key) {
-                                $.jqModel.setValue(elements[i], setter[key]);
-                                break;
+                            if (!attrName.startsWith("c-")) continue;
+
+                            var propName = element.attributes[i].value;
+
+                            if (setter[propName] === undefined) continue;
+
+                            if (attrName === "c-value" || attrName === "c-value-number") {
+                                $.jqModel.setValue(element, setter[propName]);
+                            } else if (attrName === "c-text" || attrName === "c-text-number") {
+                                $.jqModel.setText(element, setter[propName])
+                            } else if (attrName === "c-checked") {
+                                $.jqModel.setChecked(element, setter[propName]);
+                            } else if (attrName === "c-class") {
+                                $.jqModel.setClasses(element, setter[propName]);
+                            } else if (attrName.startsWith("c-class-")) {
+                                $.jqModel.setClass(element, attrName.replace("c-class-", ""), setter[propName]);
+                            } else if (attrName === "c-style") {
+                                $.jqModel.setStyles(element, setter[propName]);
+                            } else if (attrName.startsWith("c-style-")) {
+                                $.jqModel.setStyle(element, attrName.replace("c-style-", ""), setter[propName]);
                             }
                         }
-
-                        if (i < elements.length) elements.splice(i, 1);
                     });
             }
         }
