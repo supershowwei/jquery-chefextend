56430787054162c72a0c592090e3b5e30c7337f2
johnny <johnny@wantgoo.com>
2021-08-23 00:41:20 +0800

    add default.setting in $.http

--- src/jQueryChefExtend/jQueryChefExtend/jquery-http/jquery-http.js
+++ src/jQueryChefExtend/jQueryChefExtend/jquery-http/jquery-http.js
@@ -1,57 +1,66 @@
 ﻿(function ($) {
-    $.extend({
-        http: {
-            beforeSend: function (func) {
-                if (func) func();
-
-                return this;
-            },
-            get: function (url, settings) {
-                if (!(settings)) settings = {};
-                    
-                settings.method = "GET";
-
-                return $.ajax(url, settings);
-            },
-            post: function (url, data, settings) {
-                if (!(settings)) settings = {};
-                    
-                settings.method = "POST";
-                settings.data = data;
-
-                if (data.constructor === FormData) {
-                    settings.cache = false;
-                    settings.contentType = false;
-                    settings.processData = false;
-
-                    return $.ajax(url, settings);
-                }
-
-                return $.ajax(url, settings);
-            },
-            put: function (url, data, settings) {
-                if (!(settings)) settings = {};
-                    
-                settings.method = "PUT";
-                settings.data = data;
-
-                return $.ajax(url, settings);
-            },
-            patch: function (url, data, settings) {
-                if (!(settings)) settings = {};
-                    
-                settings.method = "PATCH";
-                settings.data = data;
-
-                return $.ajax(url, settings);
-            },
-            delete: function (url, settings) {
-                if (!(settings)) settings = {};
-                    
-                settings.method = "DELETE";
-
-                return $.ajax(url, settings);
-            }
+    var _http = {
+        default: {
+            settings: {}
         }
-    });
+    };
+
+    _http.beforeSend = function (func) {
+        if (func) func();
+
+        return this;
+    }
+
+    _http.get = function (url, settings) {
+        settings = $.extend(true, {}, _http.default.settings, settings);
+
+        settings.method = "GET";
+
+        return $.ajax(url, settings);
+    }
+
+    _http.post = function (url, data, settings) {
+        settings = $.extend(true, {}, _http.default.settings, settings);
+
+        settings.method = "POST";
+        settings.data = data;
+
+        if (data.constructor === FormData) {
+            settings.cache = false;
+            settings.contentType = false;
+            settings.processData = false;
+
+            return $.ajax(url, settings);
+        }
+
+        return $.ajax(url, settings);
+    }
+
+    _http.put = function (url, data, settings) {
+        settings = $.extend(true, {}, _http.default.settings, settings);
+
+        settings.method = "PUT";
+        settings.data = data;
+
+        return $.ajax(url, settings);
+    }
+
+    _http.patch = function (url, data, settings) {
+        settings = $.extend(true, {}, _http.default.settings, settings);
+
+        settings.method = "PATCH";
+        settings.data = data;
+
+        return $.ajax(url, settings);
+    }
+
+    _http.delete = function (url, settings) {
+        settings = $.extend(true, {}, _http.default.settings, settings);
+
+        settings.method = "DELETE";
+
+        return $.ajax(url, settings);
+    }
+
+    $.extend({ http: _http });
 })(jQuery);
