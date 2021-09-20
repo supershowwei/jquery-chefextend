325c594cbc9a244c2a081b4c0f315ce5624c317f
johnny <johnny@wantgoo.com>
2021-09-20 23:04:41 +0800

    jquery-http: support deferred default settings and url function

--- src/jQueryChefExtend/jQueryChefExtend/jquery-http/jquery-http.js
+++ src/jQueryChefExtend/jQueryChefExtend/jquery-http/jquery-http.js
@@ -1,4 +1,11 @@
 ﻿(function ($) {
+    var isPromise = function (obj) {
+        if (typeof obj === "object" && typeof obj.then !== "function") {
+            return false;
+        }
+        return String(obj.then) === String($.Deferred().then);
+    }
+
     var _defaultSettings;
     var _http = {
         default: {
@@ -11,56 +18,59 @@
         }
     };
 
-    _http.beforeSend = function (func) {
-        if (func) func();
-
-        return this;
-    }
-
     _http.get = function (url, settings) {
-        settings = $.extend(true, {}, _http.default.settings, settings);
-        settings.method = "GET";
-
-        return $.ajax(url, settings);
+        return (isPromise(_http.default.settings) ? _http.default.settings : $.Deferred().resolve(_http.default.settings).promise()).then(function (defaultSettings) {
+            settings = $.extend(true, {}, defaultSettings, settings);
+            settings.method = "GET";
+            return $.ajax(typeof url === "function" ? url() : url, settings);
+        });
     }
 
     _http.post = function (url, data, settings) {
-        settings = $.extend(true, {}, _http.default.settings, settings);
-        settings.method = "POST";
-        settings.data = data;
+        return (isPromise(_http.default.settings) ? _http.default.settings : $.Deferred().resolve(_http.default.settings).promise()).then(function (defaultSettings) {
+            settings = $.extend(true, {}, defaultSettings, settings);
+            settings.method = "POST";
+            settings.data = data;
 
-        if (data.constructor === FormData) {
-            settings.cache = false;
-            settings.contentType = false;
-            settings.processData = false;
+            if (data.constructor === FormData) {
+                settings.cache = false;
+                settings.contentType = false;
+                settings.processData = false;
 
-            return $.ajax(url, settings);
-        }
+                return $.ajax(typeof url === "function" ? url() : url, settings);
+            }
 
-        return $.ajax(url, settings);
+            return $.ajax(typeof url === "function" ? url() : url, settings);
+        });
     }
 
     _http.put = function (url, data, settings) {
-        settings = $.extend(true, {}, _http.default.settings, settings);
-        settings.method = "PUT";
-        settings.data = data;
+        return (isPromise(_http.default.settings) ? _http.default.settings : $.Deferred().resolve(_http.default.settings).promise()).then(function (defaultSettings) {
+            settings = $.extend(true, {}, defaultSettings, settings);
+            settings.method = "PUT";
+            settings.data = data;
 
-        return $.ajax(url, settings);
+            return $.ajax(typeof url === "function" ? url() : url, settings);
+        });
     }
 
     _http.patch = function (url, data, settings) {
-        settings = $.extend(true, {}, _http.default.settings, settings);
-        settings.method = "PATCH";
-        settings.data = data;
+        return (isPromise(_http.default.settings) ? _http.default.settings : $.Deferred().resolve(_http.default.settings).promise()).then(function (defaultSettings) {
+            settings = $.extend(true, {}, defaultSettings, settings);
+            settings.method = "PATCH";
+            settings.data = data;
 
-        return $.ajax(url, settings);
+            return $.ajax(typeof url === "function" ? url() : url, settings);
+        });
     }
 
     _http.delete = function (url, settings) {
-        settings = $.extend(true, {}, _http.default.settings, settings);
-        settings.method = "DELETE";
+        return (isPromise(_http.default.settings) ? _http.default.settings : $.Deferred().resolve(_http.default.settings).promise()).then(function (defaultSettings) {
+            settings = $.extend(true, {}, defaultSettings, settings);
+            settings.method = "DELETE";
 
-        return $.ajax(url, settings);
+            return $.ajax(typeof url === "function" ? url() : url, settings);
+        });
     }
 
     $.extend({ http: _http });
