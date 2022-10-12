eb34451451f0ba1c9200693278d35681b46a8ebb
johnny <johnny@wantgoo.com>
2022-10-12 16:34:40 +0800

    add test case Test_Contenteditable_can_Set_and_Get_value_with_Dazzle

--- test/jquery-model-tests.js
+++ test/jquery-model-tests.js
@@ -81,6 +81,17 @@ describe("jquery-model test cases", function () {
         expect(model.name).toBe("Johnny");
     });
 
+    it("Test_Contenteditable_can_Set_and_Get_value_with_Dazzle", function () {
+        var $container = $("#" + jasmine.currentTest.description);
+
+        $container.model({ id: 2, name: "Mary" });
+
+        var model = $container.model();
+
+        expect(model.id).toBe(2);
+        expect(model.name).toBe("Mary");
+    });
+
     it("Test_Set_Model_Values_use_KeyValue", function () {
         var $container = $("#" + jasmine.currentTest.description);
 
